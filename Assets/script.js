7666c4e811a3ec3b0ba6973e32f55c69c2b06cf9
austinmetcalf <austinchaz11@gmail.com>
2022-07-19 11:35:16 -0600

    cleaned up some styling of the page.

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
     var date = moment().format('YYYY-MM-DD')
-    // need to do something similar to set the calendar values if they are in local storage
+   
     $("#currentDay").text(date);
 
     var timeSlots = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
@@ -16,11 +16,11 @@ $(document).ready(function(){
     $(".saveButton").click(
         function(event) {
             console.log(event)
-            var buttonClicked = event.target; // <button>Save</button>
-            var inputField = buttonClicked.previousElementSibling // <input>
-            var inputValue = inputField.value // whatever text is in the field
+            var buttonClicked = event.target; 
+            var inputField = buttonClicked.previousElementSibling 
+            var inputValue = inputField.value 
 
-            var time = inputField.previousElementSibling.textContent // <span>9:00am</span>
+            var time = inputField.previousElementSibling.textContent 
             localStorage.setItem(time, inputValue)
             
         }
