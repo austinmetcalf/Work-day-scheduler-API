35f3e85c17924cc047ce3fa538274f5880067098
austinmetcalf <austinchaz11@gmail.com>
2022-07-19 22:12:34 -0600

    removed console logs after testing script for local storage.

--- Assets/script.js
+++ Assets/script.js
@@ -5,13 +5,14 @@ $(document).ready(function(){
     $("#currentDay").text(date);
 
     var timeSlots = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
-
-   
-
-
-    var timeSlot
+for(var i = 0 ; i <= timeSlots.length; i++){
+    var timeSlot= timeSlots[i]
     var appointment= localStorage.getItem(timeSlot)
-    $("#appointment").val(appointment);
+    timeSlot="#"+timeSlot
+    $(timeSlot).val(appointment);
+}
+   
+    
 
     $(".saveButton").click(
         function(event) {
